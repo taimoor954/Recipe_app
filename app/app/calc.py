a2e2115d00a305ad3381fd008a27b1dea592077f
taimoor954 <taimoormuhammad954@gmail.com>
2023-01-09 16:09:57 +0500

    added postgres-adapter in docker file and pyscop2g installation done

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions (Example of testing)
+"""
+
+
+def add(x,y):
+    """Add x and y and return answer"""
+    return x+y
+
+
+def subtract(x,y):
+    """subtract x and y and return answer"""
+    return x - y
